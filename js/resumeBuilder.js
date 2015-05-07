1f7bcad8ccdcc0da6562ccc9ecd0edf5f5aa995a
darthpbal <ronquillo.paul@gmail.com>
2015-05-07 14:05:12 -0700

    ffffff

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,10 @@
 var bio = {
     "name" : "Paul Ronquillo",
-    "role" : "Electrical Engineer",
+    "role" : "Electrical Engineering Student",
     "contact" : {
-        "mobile" : "555.555.5555",
+        "mobile" : "6237030508",
         "email" : "Ronquillo.Paul@gmail.com",
-        "location" : "Phoenix"
+        "home" : "8154w whyman ave"
     },
     "pic" : "http://upload.wikimedia.org/wikipedia/commons/e/ec/Silego_clock_generator.JPG",
     "welcome" : "Welcome to my resume website. Please, Make yourself at homepage...",
@@ -13,15 +13,66 @@ var bio = {
         "knowing stuff about music",
         "explaining things",
         "electronics",
-        "leadership"
+        "leadership",
+        "Computer literate",
+        "Code monkey",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Great at smoking weed",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers",
+        "Soldering",
+        "Good with a pair of pliers"
     ]
 };
 
 var work = {
-    "currentJob" : "Code Monkey",
-    "currentEmployer" : "Phoenix Community College",
-    "years" : "<1",
-    "currentCity" : "Phoenix"
+    "jobs" : [
+    {
+        "employer" : "chessemp",
+        "title" : "chess coach",
+        "location" : "scottsdale",
+        "dates" : "some time to some time",
+        "description" : "I got paid to suck at chess for a while",
+        "link" : "images/fry.jpg"
+    },
+    {
+        "employer" : "phoenix college a&R",
+        "title" : "phone answereer",
+        "location" : "phoenix",
+        "dates" : "some time to some time",
+        "description" : "I talked to stupid people on the phone",
+        "link" : "images/fry.jpg"
+    },
+    {
+        "employer" : "pc IT",
+        "title" : "codemonkey",
+        "location" : "phoenix",
+        "dates" : "now",
+        "description" : "Get paid a lot of money to suck at programming.",
+        "link" : "images/fry.jpg"
+    }
+    ]
 };
 
 var education = {
@@ -57,9 +108,49 @@ var education = {
     ]
 };
 
-var project = {
-    "electronicsPro" : "Saphhire tracker",
-    "compsci" : "flash drive note taking app",
-    "electronicsHobby" : "reel steel simple arm",
-    "mechanical" : "build 3D printer"
+var projects = {
+    "projects" : [
+    {
+        "title" : "Sapphire Tracker",
+        "dates" : "9/20/14",
+        "description" : "I was charged with the task initially of learning 
+        how to install a tracking system for a A company named Ruby Ride.
+        Eventually through a series of unsatisfactory tracking units, we decided
+        that it would be more efficient to purpose build some for the company 
+        and use those. From then on, my task was to use my current knowledge of tech
+        to develop a working and reliable tracking system for their company vehicles."
+        "images" : "http://www.sapphiretech.com/images/sapphire_corporate_logo.jpg",
+    },
+    {
+        "title" : "Flash drive note taking & organizing app",
+        "dates" : "not long ago",
+        "description" : "Basically slowly and incrementally making a flash drive 
+        note taking application as a small programming project that I work on now 
+        and then. Its end purpose is to be a way to take notes in any webbrowser 
+        on any computer and organize them. When I say organize, I mean I want to 
+        be able to write notes and the system take something like date and subject 
+        and class and be able to tag it with specifics like if that day in math, 
+        you reviewed laplace transforms, then you can tag the note group for that 
+        day as laplace transforms. In the end, what if you have something like 
+        \"test mode\" where you activate this when a test is coming up. It then 
+        opens up a window asking for the chapters and respective subjects per 
+        chapter that the test covers. This gives the application enough 
+        information to group your notes intelligently, and then you just check 
+        off what skills you feel you've mastered and the app hides them, cleaning 
+        your workspace.",
+        "images" : "http://iblog.dearbornschools.org/harrison/wp-content/uploads/sites/670/2013/12/taking_notes.jpg"
+    },
+    {
+        "title" : "Reel Steel Simple Arm",
+        "dates" : "uggghhhh",
+        "description" : "simple arm",
+        "images" : "http://www.dreamworksstudios.com/files/rs_1sht_payoff-web.jpg"
+    },
+    {
+        "title" : "Ormerod 2",
+        "dates" : "kjsugh   ",
+        "description" : "sgcflflkn  ",
+        "images" : "https://reprappro.com/wp-content/uploads/2014/05/ORM2-complete_small.png"
+    }
+    ]
 };
